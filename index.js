75cde87e30baf544d12163af3a55ea721db1dc39
saidbek <106152417+Saidbek2409@users.noreply.github.com>
2022-11-04 08:49:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+let arr2 = [[{a: {price:20}}],[{a: {price:35}}],[{a: {price:44}}],]
+let total = 0
+
+arr2.filter(num => total = arr2[0][0].a.price + arr2[1][0].a.price + arr2[2][0].a.price)
+console.log(total);
